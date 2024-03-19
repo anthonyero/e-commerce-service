ca5842f83febfdc6c944cad759765cca1b25c284
anthonyero <anthonyero@gmail.com>
2024-03-18 23:20:45 -0500

    Defined the Product model and its column with specific validation

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    productName: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        is: ^[0-9]*\.[0-9]{2}$ // Regex to match up to two decimal points. From CodePal
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        is.Int: true 
+      }
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
