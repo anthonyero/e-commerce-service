5d1ffee91b9ba6a387e64d40c0cfdf82e8a57aa0
anthonyero <anthonyero@gmail.com>
2024-03-19 00:56:20 -0500

    Removed onDelete property from Category hasMany association as it was not specified

--- models/index.js
+++ models/index.js
@@ -12,8 +12,7 @@ Product.belongsTo(Category, {
 // Categories have many Products
 
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
