c0076c1a92f6ad292b818be1f05518173b0c40f5
anthonyero <anthonyero@gmail.com>
2024-03-19 22:38:05 -0500

    Defined a GET route to retrive all tags in the Tag table and their associated products

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }] // Be mindful it is `include`, not `includes`, no error will be generated but you will not receive the product information
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err)
+  };
 });
 
 router.get('/:id', (req, res) => {
