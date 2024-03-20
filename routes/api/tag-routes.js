c09ec5a249a42826b4d7516c2cb15bbdca7a1b34
anthonyero <anthonyero@gmail.com>
2024-03-19 23:12:17 -0500

    Defined a PUT route to update a tag using its id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,8 +41,21 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateData = await Tag.update({
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(201).json(updateData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
