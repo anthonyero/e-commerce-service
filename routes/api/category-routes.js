14cab5bb04bb55e146368788ab6f26490c1b2f48
anthonyero <anthonyero@gmail.com>
2024-03-19 20:23:32 -0500

    Defined a DELETE route to remove a category from the Category table by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -58,8 +58,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteRequestData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteRequestData) {
+      res.status(400).json({ message: 'No category found with that id.' });
+      return;
+    }
+    res.status(200).json(deleteRequestData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
