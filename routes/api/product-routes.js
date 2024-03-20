3d1ef723900df12af31c592e42e3a625c288d6a1
anthonyero <anthonyero@gmail.com>
2024-03-19 22:04:42 -0500

    Defined a DELETE route to remove a product from the Product table by usings its id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,6 +33,7 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
+  // Notice that `category_id` is excluded. If we examine the model, `category_id` can be NULL
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -108,8 +109,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const deleteRequest = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteRequest){
+      res.status(404).json( {message: 'No product was found with that id.' });
+      return;
+    };
+    res.status(200).json(deleteRequest);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
